3ea97dbd8a827f9e46d2b228d75d1ef7ed8801b7
Chieh <hsuchieh1010@gmail.com>
2025-04-25 18:30:24 +0200

    ajouté bouclaire tarifaire

--- openfisca_france_entreprises/variables/consommation_energie/autres_produits.py
+++ openfisca_france_entreprises/variables/consommation_energie/autres_produits.py
@@ -30,7 +30,7 @@ class consommation_carburant_constitue_minimum_90_alcool_ethylique_agricole(Vari
 
 class consommation_essence_normale(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -46,7 +46,7 @@ class consommation_essences_speciales_utilisees_comme_carburants_combustibles(Va
 
 class consommation_gaz_naturel_etat_gazeux_utilise_sous_conditions_aux_moteurs_stationnaires(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -54,7 +54,7 @@ class consommation_gaz_naturel_etat_gazeux_utilise_sous_conditions_aux_moteurs_s
 
 class consommation_gaz_naturel_etat_gazeux_utilises_comme_carburants(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -72,7 +72,7 @@ class consommation_super_e5(Variable):
     value_type = float
     unit = 'Hectolitre'
     entity = Etablissement
-    label = ''
+    label = '''-----supercarburant d'une teneur en plomb n'excédant pas 0,005 g/ litre, autre que le supercarburant correspondant à l'indice d'identification n° 11 bis, contenant jusqu'à 5 % volume/ volume d'éthanol, 22 % volume/ volume d'éthers contenant 5 atomes de carbone ou plus, par molécule et d'une teneur en oxygène maximale de 2,7 % en masse d'oxygène ;	'''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
     definition_period = YEAR
 
@@ -80,7 +80,7 @@ class consommation_super_e10(Variable):
     value_type = float
     unit = 'Hectolitre'
     entity = Etablissement
-    label = ''
+    label = '''-----supercarburant d'une teneur en plomb n'excédant pas 0,005 g/ litre, autre que les supercarburants correspondant aux indices d'identification 11 et 11 bis, et contenant jusqu'à 10 % volume/ volume d'éthanol, 22 % volume/ volume d'éthers contenant 5 atomes de carbone, ou plus, par molécule et d'une teneur en oxygène maximale de 3,7 % en masse/ masse d'oxygène ;'''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
     definition_period = YEAR
 
@@ -102,7 +102,7 @@ class consommation_super_plombe(Variable):
 
 class consommation_propane_carburants_autres_100kg_nets(Variable): 
     value_type = float
-    unit = ''
+    unit = '100kg nets'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -110,7 +110,7 @@ class consommation_propane_carburants_autres_100kg_nets(Variable):
 
 class consommation_propane_carburants_sous_conditions_100kg_nets(Variable): 
     value_type = float
-    unit = ''
+    unit = '100kg nets'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -118,7 +118,7 @@ class consommation_propane_carburants_sous_conditions_100kg_nets(Variable):
 
 class consommation_propane_carburants_usages_autres_que_comme_carburant_100kg_nets(Variable): 
     value_type = float
-    unit = ''
+    unit = '100kg nets'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -194,7 +194,7 @@ class consommation_gazole(Variable):
     value_type = float
     unit = 'Hectolitre'
     entity = Etablissement
-    label = ''
+    label = '''indice 22; ----autres, à l'exception du gazole coloré et tracé en apllication du a du 1 de l'article 265 B ;'''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
     definition_period = YEAR
 
@@ -224,7 +224,7 @@ class consommation_fioul_lourd_hts_100kg(Variable):
 
 class consommation_point_eclair_inferieur_120deg_c_hectolitre(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -232,7 +232,7 @@ class consommation_point_eclair_inferieur_120deg_c_hectolitre(Variable):
 
 class consommation_emulsion_eau_gazole_autres_hectolitre(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -240,7 +240,7 @@ class consommation_emulsion_eau_gazole_autres_hectolitre(Variable):
 
 class consommation_emulsion_eau_gazole_sous_conditions_hectolitre(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -265,7 +265,7 @@ class consommation_carburateurs_essence_carburants_avion_hL(Variable):
 
 class consommation_carburateurs_essence_sous_conditions_hL(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -273,7 +273,7 @@ class consommation_carburateurs_essence_sous_conditions_hL(Variable):
 
 class consommation_carburateurs_petrole_lampant_autres_hL(Variable): 
     value_type = float
-    unit = '100 kg nets'
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -281,7 +281,7 @@ class consommation_carburateurs_petrole_lampant_autres_hL(Variable):
 
 class consommation_carburateurs_petrole_lampant_carburant_moteurs_avion_hL(Variable): 
     value_type = float
-    unit = '100 kg nets'
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -289,7 +289,7 @@ class consommation_carburateurs_petrole_lampant_carburant_moteurs_avion_hL(Varia
 
 class consommation_carburateurs_petrole_lampant_sous_conditions_hL(Variable): 
     value_type = float
-    unit = ''
+    unit = 'Hectolitre'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -329,7 +329,7 @@ class consommation_autres_gaz_petrole_liquefies_utilises_comme_carburants_autres
 
 class consommation_autres_gaz_petrole_liquefies_utilises_comme_carburants_sous_condition_100kg(Variable): 
     value_type = float
-    unit = ''
+    unit = '100 kg nets'
     entity = Etablissement
     label = ''
     reference = 'https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000041505082/2025-04-16'
@@ -357,6 +357,7 @@ class consommation_autres_produits_energetique_ticc(Variable):
         return totale
     
     def formula_2012_01_01(etablissement, period, parameters):
+        #par rapport à précédement, ajouté consommation_melanges_hydrocarbures_aromatiques
         totale = etablissement('consommation_melanges_hydrocarbures_aromatiques', period) + etablissement('consommation_huiles_lubrifiantes_et_autres', period) + etablissement('consommation_vaseline', period) + etablissement('consommation_paraffine_moins_soixante_quinze_pourcent_huile', period) + etablissement('consommation_paraffine_cires_de_petrole_residus_paraffineux', period) + etablissement('consommation_bitumes_de_petrole', period) + etablissement('consommation_autres_residus_huiles_petrole_ou_mineraux_bitumineux', period) + etablissement('consommation_melanges_bitumeux', period) + etablissement('consommation_preparations_traitement_matiere_textiles', period) + etablissement('consommation_preparations_lubrifiantes', period) + etablissement('consommation_additifs_huiles_lubrifiantes', period)
         etablissement('', period)
 
@@ -480,9 +481,9 @@ class consommation_additifs_huiles_lubrifiantes(Variable):
 #     definition_period = YEAR
 
     
-
+#pour après 2022, l'accise
 #tecpe
-class consommation_gaz_carburant(Variable): #renommer à consommation_gaz_carburant
+class consommation_gaz_carburant(Variable): 
     value_type = float
     unit = 'MWh'
     entity = Etablissement
