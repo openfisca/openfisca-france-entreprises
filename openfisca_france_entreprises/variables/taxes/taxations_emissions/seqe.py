1386d72d7e19bf662e1c5b7bb827ca3b7f46986c
ArthurBidel <arthurbidel.ab@gmail.com>
2026-07-02 17:03:24 +0200

    Ajout de description

--- openfisca_france_entreprises/variables/taxes/taxations_emissions/seqe.py
+++ openfisca_france_entreprises/variables/taxes/taxations_emissions/seqe.py
@@ -1,6 +1,15 @@
 from openfisca_core.model_api import Variable, YEAR
 from openfisca_france_entreprises.entities import Etablissement, UniteLegale
 
+"""
+SEQE (Système d'échange de quotas d'émission) : Montant payé par l'entreprise en quotas via le SEQE*
+
+Hypothèses : 
+- Les entreprises achètent tous les quotas nécessaires, elles ne payent pas d'amendes pour manquement.
+- Les prix retenus sont des moyennes annuelles pondérées par les volumes de vente.
+- Les entreprises vendent leurs quotas gratuits et engrengent une plus value, ainsi le résultat est potentiellement négatif.
+"""
+
 class seqe_price_paid(Variable):
     value_type = float
     entity = Etablissement
